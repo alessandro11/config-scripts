3933079af72dce64e60ddd542d9318c7ff39d9c7
Eduardo L. Buratti <elb09@c3sl.ufpr.br>
2013-02-19 19:02:38 -0300

    Fix load and uptime and add a class for sysinfo
    Signed-off-by: Eduardo L. Buratti <elb09@c3sl.ufpr.br>

--- dock/sysinfo.py
+++ dock/sysinfo.py
@@ -7,140 +7,147 @@ import time
 from datetime import datetime
 import psutil
 import sensors
+import re
 
-PATH = os.path.abspath(os.path.dirname(__file__) + "/../")
-sys.path.append(PATH)
+class SysInfoDock:
 
-from config import *
+    def __init__(self):
+        if not 'FONT' in globals():
+            raise Exception('Config not loaded, missing \'from config import *\'?')
 
-CPUSENSOR = None
+        sensors.init()
 
-LASTSENT = None
-LASTRECV = None
-LASTTIMESTAMP = None
+        for chip in sensors.iter_detected_chips():
+            for feature in chip:
+                if feature.label[:4] == 'temp':
+                    self.cpusensor = feature
+                    break
 
-def get_cpu_freq():
-    try:
-        cpuinfo = open('/proc/cpuinfo', 'r')
-        for l in cpuinfo.readlines():
-            if l[:7] == 'cpu MHz':
-                cpufreq = float(l.split(':')[1].strip())
-                break
+        self.lastsent = None
+        self.lastrecv = None
+        self.lasttimestamp = None
 
-        cpuinfo.close()
+        self.re_uptime = re.compile('^ .* up (?P<uptime>.*),  [0-9]* user,  load average: (?P<load>.*)$')
 
-        return cpufreq / 1000
-    except:
-        return 0.0
+    def run(self):
+        args = [ "-x", "600",
+                 "-y", "0",
+                 "-w", "1120",
+                 "-h", str(HEIGHT),
+                 "-ta", "l",
+                 "-bg", COLOR['BG'],
+                 "-fg", COLOR['FG'],
+                 "-fn", FONT ]
 
-def get_cpu_temp():
-    if CPUSENSOR:
-        return CPUSENSOR.get_value()
-    else:
-        return 0.0
+        dzen = subprocess.Popen(['dzen2'] + args, stdin=subprocess.PIPE)
 
-def get_uptime_and_load():
-    res = subprocess.check_output(['uptime'])
-    fields = res.strip().split()
+        while 1:
+            dzen.stdin.write(self.get_data() + '\n')
+            time.sleep(1)
 
-    uptime = fields[0]
-    load = (fields[7] + fields[8] + fields[9]).replace(',', ' ')
+    def get_data(self):
+        ret = " "
 
-    return (uptime, load)
+        cpu = psutil.cpu_percent(interval=0)
+        cpufreq = self.get_cpu_freq()
+        cputemp = self.get_cpu_temp()
+        ret += title("cpu") + icon("cpu", fg=COLOR['MAGENTA2'])
+        ret += text("%.2fGHz " % cpufreq)
+        ret += progress(int(cpu), fg=COLOR['MAGENTA'])
+        if cputemp > 60:
+            ret += text("%dc" % int(cputemp), fg=COLOR['RED2'])
+        else:
+            ret += text("%dc" % int(cputemp))
+        ret += sep()
 
-def get_gpu_temp():
-    res = subprocess.check_output(['nvidia-settings', '-q', 'gpucoretemp'])
-    temp = res.split('\n')[1].split(': ')[1].replace('.', '')
+        (uptime, load) = self.get_uptime_and_load()
+        ret += title("load") + text("%s" % load)
+        ret += sep()
+        ret += title("uptime") + text("%s" % uptime)
+        ret += sep()
 
-    return int(temp.strip())
+        mem = psutil.virtual_memory().percent
+        ret += title("mem") + icon("mem", fg=COLOR['GREEN2'])
+        ret += progress(int(mem), fg=COLOR['GREEN2'])
+        ret += sep()
 
-def get_net_speed():
-    global LASTTIMESTAMP, LASTRECV, LASTSENT
+        gputemp = self.get_gpu_temp()
+        ret += title("gpu")
+        if gputemp > 60:
+            ret += text("%dc" % gputemp, fg=COLOR['RED2'])
+        else:
+            ret += text("%dc" % gputemp)
+        ret += sep()
 
-    eth0 = psutil.network_io_counters(pernic=True)['eth0']
-    now = long(datetime.now().strftime('%s'))
-    downspeed = 0.0
-    upspeed = 0.0
+        disk = psutil.disk_usage('/').percent
+        ret += title("disk") + icon("fs_01", fg=COLOR['YELLOW'])
+        ret += progress(int(disk), fg=COLOR['YELLOW'])
+        ret += sep()
 
-    if LASTTIMESTAMP:
-        downspeed = (eth0.bytes_recv - LASTRECV) / (now - LASTTIMESTAMP) / 1024.0
-        upspeed = (eth0.bytes_sent - LASTSENT) / (now - LASTTIMESTAMP) / 1024.0
+        (downspeed, upspeed) = self.get_net_speed()
+        ret += title("net")
+        ret += icon("net_down_03", fg=COLOR['GREEN2'])
+        ret += text("%.1f " % downspeed)
+        ret += icon("net_up_03", fg=COLOR['RED2'])
+        ret += text("%.1f " % upspeed)
 
-    LASTRECV = eth0.bytes_recv
-    LASTSENT = eth0.bytes_sent
-    LASTTIMESTAMP = now
+        return ret
+    def get_cpu_freq(self):
+        try:
+            cpuinfo = open('/proc/cpuinfo', 'r')
+            for l in cpuinfo.readlines():
+                if l[:7] == 'cpu MHz':
+                    cpufreq = float(l.split(':')[1].strip())
+                    break
 
-    return (downspeed, upspeed)
+            cpuinfo.close()
 
-def get_data():
-    ret = " "
+            return cpufreq / 1000
+        except:
+            return 0.0
 
-    cpu = psutil.cpu_percent(interval=0)
-    cpufreq = get_cpu_freq()
-    cputemp = get_cpu_temp()
-    ret += title("cpu") + icon("cpu", fg=COLOR['MAGENTA2'])
-    ret += text("%.2fGHz " % cpufreq)
-    ret += progress(int(cpu), fg=COLOR['MAGENTA'])
-    if cputemp > 60:
-        ret += text("%dc" % int(cputemp), fg=COLOR['RED2'])
-    else:
-        ret += text("%dc" % int(cputemp))
-    ret += sep()
+    def get_cpu_temp(self):
+        if self.cpusensor:
+            return self.cpusensor.get_value()
+        else:
+            return 0.0
 
-    (uptime, load) = get_uptime_and_load()
-    ret += title("load") + text("%s" % load)
-    ret += sep()
-    ret += title("uptime") + text("%s" % uptime)
-    ret += sep()
+    def get_uptime_and_load(self):
+        res = self.re_uptime.match(subprocess.check_output(['uptime']))
 
-    mem = psutil.virtual_memory().percent
-    ret += title("mem") + icon("mem", fg=COLOR['GREEN2'])
-    ret += progress(int(mem), fg=COLOR['GREEN2'])
-    ret += sep()
+        uptime = res.group('uptime')
+        load = res.group('load').replace(',', '')
 
-    gputemp = get_gpu_temp()
-    ret += title("gpu")
-    if gputemp > 60:
-        ret += text("%dc" % gputemp, fg=COLOR['RED2'])
-    else:
-        ret += text("%dc" % gputemp)
-    ret += sep()
+        return (uptime, load)
 
-    disk = psutil.disk_usage('/').percent
-    ret += title("disk") + icon("fs_01", fg=COLOR['YELLOW'])
-    ret += progress(int(disk), fg=COLOR['YELLOW'])
-    ret += sep()
+    def get_gpu_temp(self):
+        res = subprocess.check_output(['nvidia-settings', '-q', 'gpucoretemp'])
+        temp = res.split('\n')[1].split(': ')[1].replace('.', '')
 
-    (downspeed, upspeed) = get_net_speed()
-    ret += title("net")
-    ret += icon("net_down_03", fg=COLOR['GREEN2'])
-    ret += text("%.1f " % downspeed)
-    ret += icon("net_up_03", fg=COLOR['RED2'])
-    ret += text("%.1f " % upspeed)
+        return int(temp.strip())
 
-    return ret
+    def get_net_speed(self):
+        eth0 = psutil.network_io_counters(pernic=True)['eth0']
+        now = long(datetime.now().strftime('%s'))
+        downspeed = 0.0
+        upspeed = 0.0
+
+        if self.lasttimestamp:
+            downspeed = (eth0.bytes_recv - self.lastrecv) / (now - self.lasttimestamp) / 1024.0
+            upspeed = (eth0.bytes_sent - self.lastsent) / (now - self.lasttimestamp) / 1024.0
+
+        self.lastrecv = eth0.bytes_recv
+        self.lastsent = eth0.bytes_sent
+        self.lasttimestamp = now
+
+        return (downspeed, upspeed)
+
 
 if __name__=="__main__":
+    PATH = os.path.abspath(os.path.dirname(__file__) + "/../")
+    sys.path.append(PATH)
 
-    sensors.init()
+    from config import *
 
-    for chip in sensors.iter_detected_chips():
-        for feature in chip:
-            if feature.label[:4] == 'temp':
-                CPUSENSOR = feature
-                break
-
-    args = [ "-x", "600",
-             "-y", "0",
-             "-w", "1120",
-             "-h", str(HEIGHT),
-             "-ta", "l",
-             "-bg", COLOR['BG'],
-             "-fg", COLOR['FG'],
-             "-fn", FONT ]
-
-    dzen = subprocess.Popen(['dzen2'] + args, stdin=subprocess.PIPE)
-
-    while 1:
-        dzen.stdin.write(get_data() + '\n')
-        time.sleep(1)
+    dock = SysInfoDock()
+    dock.run()
