2f4308f2b257acfd4136e5d273ea9bc8018b9ee6
Alessandro Elias <ae11@c3sl.ufpr.br>
2017-01-25 14:13:00 -0200

    Set animation of double colon blinking.
    Signed-off-by: Alessandro Elias <ae11@c3sl.ufpr.br>

--- dock/clock.py
+++ dock/clock.py
@@ -12,8 +12,9 @@ from config import *
 
 if __name__=="__main__":
 
-    fmt = '%d^fg(#444).^fg()%m^fg(#444).^fg()%y^fg(#007b8c)/^fg(#5f656b)%a ^fg(#a488d9)| ^fg()%H^fg(#444):^fg()%M^fg(#444):^fg()%S'
+    change_color = 1
 
+    fmt = '^fg(#5f656b)%a ^fg(#007b8c)%d ^fg()%h^fg(#444) ^fg()%Y^fg(#007b8c) ^fg(#a488d9)| ^fg()%H^fg(#444):^fg()%M^fg(#444):^fg()%S'
     args = [ "-x", DOCK_POS['CLx'],
              "-y", "0",
              "-w", DOCK_POS['CLw'],
@@ -28,3 +29,9 @@ if __name__=="__main__":
     while 1:
         dzen.stdin.write(arrows() + time.strftime(fmt) + ' \n')
         time.sleep(1)
+        if change_color == True:
+            color = '#ffffffff'
+        else:
+            color = '#444'
+        change_color = ~change_color
+        fmt = '^fg(#5f656b)%a ^fg(#007b8c)%d ^fg()%h^fg(#444) ^fg()%Y^fg(#007b8c) ^fg(#a488d9)| ^fg()%H^fg(' + color + '):^fg()%M^fg(' + color + '):^fg()%S'
